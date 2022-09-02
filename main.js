e23dc28c8b71572d7beb976eee6aef5d2949c2f7
Mathews <mathews.milimu@student.moringaschool.com>
2022-09-02 23:38:53 +0300

    5

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-
+const options = {
+	method: 'GET',
+	headers: {
+		'Content_Type': "application/json"
+	},
+    body : JSON.stringify({
+        brand: ""
+    })
+};
 
 fetch('https://shoe-catalogue-api-codex.herokuapp.com/api/shoes/brand/Nike')
 	.then(response => response.json())
